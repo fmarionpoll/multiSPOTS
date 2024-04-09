ce4cfa97c5c22612533d821bb2e3346585bd3887
Frederic Marion-Poll <marion_poll@hotmail.com>
2024-04-09 18:11:31 +0200

    update time in dialog title

--- src/main/java/plugins/fmp/multiSPOTS/MultiSPOTS.java
+++ src/main/java/plugins/fmp/multiSPOTS/MultiSPOTS.java
@@ -21,7 +21,7 @@ import plugins.fmp.multiSPOTS.dlg.spots.MCSpots_;
 
 public class MultiSPOTS extends PluginActionable  
 {
-	public IcyFrame 		mainFrame 		= new IcyFrame("multiSPOTS April 8, 2024", true, true, true, true);
+	public IcyFrame 		mainFrame 		= new IcyFrame("multiSPOTS April 9, 2024", true, true, true, true);
 	public ExperimentCombo 	expListCombo 	= new ExperimentCombo();
 	
 	public MCExperiment_ 	paneExperiment 	= new MCExperiment_();
